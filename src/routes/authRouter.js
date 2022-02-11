aea8881ea826adbf2b33eed1a7ac63fc13e8ba43
mateuskuritza <mateuskuritza@gmail.com>
2022-02-11 10:33:24 -0300

    Preparando revisão de código

--- src/routes/authRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { Login, signUp } from '../controllers/authController.js';
-import { userSchemaValidationMiddleware } from '../middlewares/schemaValidationMiddleware.js';
-
-const authRouter = express.Router();
-authRouter.post('/auth/sign-up',userSchemaValidationMiddleware, signUp);
-authRouter.post('/auth/login', Login);
-export default authRouter;
